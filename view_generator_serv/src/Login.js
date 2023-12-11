b8f79a54b484f93acaaf7d15f948d4ed3031f293
Bilal <muhammad.bilal.3446574@gmail.com>
2023-12-11 09:46:09 +0500

    Added login/registration page

---
+++ view_generator_serv/src/Login.js
@@ -0,0 +1,177 @@
+import React, { useState } from "react";
+
+import "./Login.css";
+
+
+// function Login() {
+//   // React States
+//   const [errorMessages, setErrorMessages] = useState({});
+//   const [isSubmitted, setIsSubmitted] = useState(false);
+
+//   // User Login info
+//   const database = [
+//     {
+//       username: "user1",
+//       password: "pass1"
+//     },
+//     {
+//       username: "user2",
+//       password: "pass2"
+//     }
+//   ];
+
+//   const errors = {
+//     uname: "invalid username",
+//     pass: "invalid password"
+//   };
+
+//   const handleSubmit = (event) => {
+//     //Prevent page reload
+//     event.preventDefault();
+
+//     var { uname, pass } = document.forms[0];
+
+//     // Find user login info
+//     const userData = database.find((user) => user.username === uname.value);
+
+//     // Compare user info
+//     if (userData) {
+//       if (userData.password !== pass.value) {
+//         // Invalid password
+//         setErrorMessages({ name: "pass", message: errors.pass });
+//       } else {
+//         setIsSubmitted(true);
+//       }
+//     } else {
+//       // Username not found
+//       setErrorMessages({ name: "uname", message: errors.uname });
+//     }
+//   };
+
+//   /
+//     }
+function Login() {
+    // React States
+    const [errorMessages, setErrorMessages] = useState({});
+    const [isSubmitted, setIsSubmitted] = useState(false);
+    const [isSignUp, setIsSignUp] = useState(false);
+
+
+  const renderErrorMessage = (name) =>
+    name === errorMessages.name && (
+      <div className="error">{errorMessages.message}</div>
+    );
+    // User Login info
+    const database = [
+        {
+            username: "user1",
+            password: "pass1"
+        },
+        {
+            username: "user2",
+            password: "pass2"
+        }
+    ];
+
+    const errors = {
+        uname: "invalid username",
+        pass: "invalid password"
+    };
+
+    const handleSubmit = (event) => {
+        //Prevent page reload
+        event.preventDefault();
+
+        var { uname, pass } = document.forms[0];
+
+        // Find user login info
+        const userData = database.find((user) => user.username === uname.value);
+
+        // Compare user info
+        if (userData) {
+            if (userData.password !== pass.value) {
+                // Invalid password
+                setErrorMessages({ name: "pass", message: errors.pass });
+            } else {
+                setIsSubmitted(true);
+            }
+        } else {
+            // Username not found
+            setErrorMessages({ name: "uname", message: errors.uname });
+        }
+    };
+
+    const handleToggleForm = () => {
+        setIsSignUp(!isSignUp);
+        setErrorMessages({});
+    };
+
+    // JSX code for login form
+    const renderLoginForm = (
+        <div className="form">
+            <form onSubmit={handleSubmit}>
+                <div className="input-container">
+                    <label>Username </label>
+                    <input type="text" name="uname" required />
+                    {renderErrorMessage("uname")}
+                </div>
+                <div className="input-container">
+                    <label>Password </label>
+                    <input type="password" name="pass" required />
+                    {renderErrorMessage("pass")}
+                </div>
+                <div className="button-container">
+                    <input type="submit" value="Sign In" />
+                </div>
+            </form>
+            <div className="toggle-form">
+                Not a user?{" "}
+                <button onClick={handleToggleForm}>Sign up</button>
+            </div>
+        </div>
+    );
+
+    // JSX code for registration form
+    const renderRegistrationForm = (
+        <div className="form">
+            <form onSubmit={handleSubmit}>
+                <div className="input-container">
+                    <label>Username </label>
+                    <input type="text" name="uname" required />
+                    {renderErrorMessage("uname")}
+                </div>
+                <div className="input-container">
+                    <label>Password </label>
+                    <input type="password" name="pass" required />
+                    {renderErrorMessage("pass")}
+                </div>
+                <div className="button-container">
+                    <input type="submit" value="Sign Up" />
+                </div>
+            </form>
+            <div className="toggle-form">
+                Already a user?{" "}
+                <button onClick={handleToggleForm}>Sign in</button>
+            </div>
+        </div>
+    );
+
+    
+
+    return (
+        <div className="app">
+            <div className="login-form">
+                <div className="title">{isSignUp ? "Sign Up" : "Sign In"}</div>
+                {isSubmitted ? (
+                    <div>User is successfully logged in</div>
+                ) : isSignUp ? (
+                    renderRegistrationForm
+                ) : (
+                    renderLoginForm
+                )}
+            </div>
+        </div>
+    );
+}
+
+export default Login;
