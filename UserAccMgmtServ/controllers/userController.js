9676dad5afc6d8350b9ed481bb38d0c8f052cc01
itsAdee <adeelahmedqureshi02@gmail.com>
2023-12-11 22:56:21 +0500

    Modularized the code

---
+++ UserAccMgmtServ/controllers/userController.js
@@ -0,0 +1,30 @@
+
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const User = require('../models/user');
+const multer = require('multer');
+const upload = multer();
+
+router.post('/register', upload.none(), async (req, res) => {
+  const { username, password, email } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const newUser = new User({ username, password: hashedPassword, email });
+  await newUser.save();
+  console.log(`User ${username} created.`);
+  res.status(201).send();
+});
+
+router.post('/login', upload.none(), async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+  if (user && await bcrypt.compare(password, user.password)) {
+    console.log(`User ${username} logged in.`);
+    res.status(200).send();
+  } else {
+    console.log(`User ${username} failed to log in.`);
+    res.status(403).send();
+  }
+});
+
+module.exports = router;
--- UserAccMgmtServ/index.js
+++ UserAccMgmtServ/index.js
@@ -1,29 +1,17 @@
+// index.js
 require("dotenv").config();
-
-
 const express = require("express");
 const bodyParser = require("body-parser");
-const { randomBytes } = require("crypto");
 const cors = require("cors");
-const axios = require("axios");
 const db = require('mongoose');
-const bcrypt = require('bcrypt');
+const userRoutes = require('./controllers/userController');
 
 const app = express();
 app.use(bodyParser.json());
-app.use(cors());
-const multer = require('multer');
-const upload = multer()
-
-
-const userSchema = new db.Schema({
-  username: String,
-  password: String,
-  email: String
-});
-
-
-const User = db.model('User', userSchema);
+app.use(cors({
+  origin: 'http://localhost:3000', // replace with the origin of your client
+  credentials: true
+}));
 
 db.connect(process.env.MONGO_URI)
   .then((result) => {
@@ -31,35 +19,10 @@ db.connect(process.env.MONGO_URI)
     // Listen for Requests
     app.listen(4003, () => {
       console.log("UserAccMgmtServ: Listening on 4003");
-    
     });
   })
   .catch((err) => {
     console.error(err);
   });
 
-
-
-
-  app.post('/register', upload.none(), async (req, res) => {
-    const { username, password, email } = req.body;
-    const hashedPassword = await bcrypt.hash(password, 10);
-    console.log(req.body)
-    const newUser = new User({ username, password: hashedPassword, email });
-    await newUser.save();
-    console.log(`User ${username} created.`);
-    res.status(201).send();
-  });
-  
-  app.post('/login', upload.none(), async (req, res) => {
-    const { username, password } = req.body;
-    const user = await User.findOne({ username });
-    if (user && await bcrypt.compare(password, user.password)) {
-      console.log(`User ${username} logged in.`);
-      res.status(200).send();
-    } else {
-      console.log(`User ${username} failed to log in.`);
-      res.status(403).send();
-    }
-  });
-
+app.use('/', userRoutes);
