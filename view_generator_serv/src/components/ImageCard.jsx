562827655191d918375ee648bd183beb63354c09
Muhammad Bilal <mbilal.bese21seecs@seecs.edu.pk>
2023-12-23 13:21:52 +0500

    Removed unnecessary CSS file and added Chakra styles

--- view_generator_serv/src/components/ImageCard.jsx
+++ view_generator_serv/src/components/ImageCard.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
-import "./css/styles.css";
+import { Box, Button, Image } from '@chakra-ui/react';
 
 function ImageCard(props) {
   return (
-    <div className="image-container">
-        <img src={props.imageUri} alt={props.imageName} />
-        
-        <button onClick={() => props.onDelete(props.id)}><img src={require("./images/delete.png")} alt="delete icon" style={{width: "20px", cursor: "pointer"}}/></button>
-    </div>
-  )
+    <Box
+      className="image-container"
+      flex="0 0 calc(20% - 10px)"
+      padding="10px"
+      border="2px solid #01d28e"
+      marginLeft="10px"
+      marginTop="10px"
+      marginRight="10px"
+    >
+      <Image src={props.imageUri} alt={props.imageName} width="300px" />
+
+      <Button onClick={() => props.onDelete(props.id)} mt="2" colorScheme="red">
+        <Image src={require('./images/delete.png')} alt="delete icon" width="20px" cursor="pointer" />
+      </Button>
+    </Box>
+  );
 }
 
-export default ImageCard
+export default ImageCard;
